1c20be99d72fe2db47a26bc052484dc0bde43609
Masayuki-Suzuki <m.suzuki.fp@gmail.com>
2020-08-31 16:07:43 -0700

    feat: Updated for v3.x ✨

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // we use requestAnimationFrame to be called by the browser before every repaint
 let requestAnimationFrame;
 
+export const ScrollSymbol = Symbol();
+
 // Get the top position of an element in the document
-const getTop = function(element, start) {
+const getTop = function (element, start) {
   // return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
-  if (element.nodeName === 'HTML') return -start;
+  if (element.nodeName === "HTML") return -start;
   return element.getBoundingClientRect().top + start;
 };
 
@@ -18,21 +20,30 @@ function getDefaultConfig() {
   };
 }
 
-const smoothScrollCtx = Symbol('smoothScrollCtx')
+const smoothScrollCtx = Symbol("smoothScrollCtx");
 
-function _smoothScroll({ scrollTo, offset, duration, container, updateHistory, hash, easingFunction }) {
+function _smoothScroll({
+  scrollTo,
+  offset,
+  duration,
+  container,
+  updateHistory,
+  hash,
+  easingFunction,
+}) {
   if (!requestAnimationFrame) {
-    requestAnimationFrame = window.requestAnimationFrame ||
-    function(fn) {
-      window.setTimeout(fn, 16);
-    };
+    requestAnimationFrame =
+      window.requestAnimationFrame ||
+      function (fn) {
+        window.setTimeout(fn, 16);
+      };
   }
 
   // Using the history api to solve issue: back doesn't work
   // most browser don't update :target when the history api is used:
   // THIS IS A BUG FROM THE BROWSERS.
-  if (updateHistory && window.history.pushState && location.hash !== hash) window.history.pushState('', '', hash);
-
+  if (updateHistory && window.history.pushState && location.hash !== hash)
+    window.history.pushState("", "", hash);
 
   const startPoint = container.scrollTop || window.pageYOffset;
   // Get the top position of an element in the document
@@ -43,84 +54,104 @@ function _smoothScroll({ scrollTo, offset, duration, container, updateHistory, h
   end += offset;
 
   const clock = Date.now();
-  const step = function() {
+  const easeFn =
+    (typeof easingFunction === "function" && easingFunction) || easeInOutCubic;
+  const step = function () {
     // the time elapsed from the beginning of the scroll
     const elapsed = Date.now() - clock;
     // calculate the scroll position we should be in
     let position = end;
-    let easeFn = (typeof easingFunction === 'function' && easingFunction) || easeInOutCubic;
     if (elapsed < duration) {
       position = startPoint + (end - startPoint) * easeFn(elapsed / duration);
 
       requestAnimationFrame(step);
     } else if (updateHistory) {
-      location.replace('#' + scrollTo.id);
+      location.replace("#" + scrollTo.id);
       // this will cause the :target to be activated.
     }
 
-    container === window ? container.scrollTo(0, position) : (container.scrollTop = position);
+    container === window
+      ? container.scrollTo(0, position)
+      : (container.scrollTop = position);
   };
   step();
 }
 
 const VueSmoothScroll = {
   install(Vue, config) {
-    Vue.directive('smooth-scroll', {
-      inserted(el, binding, vnode) {
+    Vue.directive("smooth-scroll", {
+      mounted(el, binding, vnode) {
         // Do not initialize smoothScroll when running server side, handle it in client
         // We do not want this script to be applied in browsers that do not support those
         // That means no smoothscroll on IE9 and below.
-        if (typeof window !== 'object' || window.pageYOffset === undefined) return;
+        if (typeof window !== "object" || window.pageYOffset === undefined)
+          return;
 
         let resolvedConfig = Object.assign({}, getDefaultConfig());
         if (config) {
           Object.assign(resolvedConfig, config);
         }
 
-        let { duration, offset, container, updateHistory, easingFunction } = binding.value || {};
+        let { duration, offset, container, updateHistory, easingFunction } =
+          binding.value || {};
         duration = duration || resolvedConfig.duration;
         offset = offset || resolvedConfig.offset;
         container = container || resolvedConfig.container;
-        updateHistory = updateHistory !== undefined ? updateHistory : resolvedConfig.updateHistory;
+        updateHistory =
+          updateHistory !== undefined
+            ? updateHistory
+            : resolvedConfig.updateHistory;
         easingFunction = easingFunction || resolvedConfig.easingFunction;
 
-        if (typeof container === 'string') {
+        if (typeof container === "string") {
           container = document.querySelector(container);
         }
 
-        const clickHandler = function(ev) {
+        const clickHandler = function (ev) {
           ev.preventDefault();
-          const hash = vnode.data.attrs.href;
+          // const hash = vnode.data.attrs.href
+          const hash = vnode.el.hash;
           const scrollTo = document.getElementById(hash.substring(1));
           if (!scrollTo) return; // Do not scroll to non-existing node
 
-          _smoothScroll({ scrollTo, offset, duration, container, updateHistory, hash, easingFunction });
-        }
+          _smoothScroll({
+            scrollTo,
+            offset,
+            duration,
+            container,
+            updateHistory,
+            hash,
+            easingFunction,
+          });
+        };
         // Attach the smoothscroll function
-        el.addEventListener('click', clickHandler);
+        el.addEventListener("click", clickHandler);
 
         el[smoothScrollCtx] = {
-          clickHandler
-        }
+          clickHandler,
+        };
       },
-      unbind(el) {
-        el.removeEventListener('click', el[smoothScrollCtx].clickHandler)
-        el[smoothScrollCtx] = null
-      }
+      unmounted(el) {
+        el.removeEventListener("click", el[smoothScrollCtx].clickHandler);
+        el[smoothScrollCtx] = null;
+      },
     });
 
-    Vue.prototype.$smoothScroll = (args) => {
+    Vue.config.globalProperties.$smoothScroll = (args) => {
+      console.log(args);
       const resolvedArgs = Object.assign({}, getDefaultConfig(), config, args);
-      return _smoothScroll(resolvedArgs)
-    }
-  }
+      return _smoothScroll(resolvedArgs);
+    };
+
+    Vue.provide(ScrollSymbol, Vue.config.globalProperties.$smoothScroll);
+  },
 };
 
-export default VueSmoothScroll
+export default VueSmoothScroll;
 
 /* istanbul ignore if */
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(VueSmoothScroll)
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.use(VueSmoothScroll);
 }
 
 /**
